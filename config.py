a83f2fdbf080198d1fee0854a87e7401980adefe
DoodyShark <da2863@nyu.edu>
2026-02-13 21:52:23 +0000

    Fixed src/misinformation-prediction/data not being tracked

--- config.py
+++ config.py
@@ -1,9 +1,9 @@
 from pathlib import Path
 
-BASE_PATH = Path("/content/drive/MyDrive/MASAID/AI_Project_Course/Misinformation/repo/week_2/sessions/")
+BASE_PATH = Path("/content/drive/MyDrive/MASAID/AI_Project_Course/Misinformation/repo-public/")
 
-DATA_PATH = BASE_PATH / Path("../../data")
+DATA_PATH = BASE_PATH / Path("data")
 
-SRC_PATH = BASE_PATH / Path("../../src")
+SRC_PATH = BASE_PATH / Path("src")
 
 SAVED_CHECKPOINTS_PATH = DATA_PATH / Path("saved_checkpoints/")
